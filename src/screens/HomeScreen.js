cace50b2870518c7fe120b26f3603875714dce42
malibuyukbudak <malibuyukbudak@gmail.com>
2021-12-29 17:23:23 +0300

    initial commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+import axios from 'axios'
+
+const[result,setResults] = useState([]);
+
+axios.get("https://api.collectapi.com/health/dutyPharmacy?il=Edirne" ,{
+  headers:{
+    "authorization":"apikey 09Erq76IT2T7OZEKzWWtOM:37wKkCNdmJxnHAgn2Y0PxN",
+    "content-type":"application/json"
+  }
+}).then(data =>data.data.result)
+.then(this.setResults(data));
+
+const {DutyPharmacyResult} = this.result;
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return <Text style={styles.text}>{DutyPharmacyResult}</Text>;
 };
 
 const styles = StyleSheet.create({
